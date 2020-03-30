4c0c80078597502f3ac736e1f8c7b41b2e382f5e
Dane Barentine <dane.barentine@quest.com>
2020-03-29 22:34:38 -0700

    Fix thermostat hold status and removed heat setpoint workaround. Update full thermostat after command.

--- thermostat.py
+++ thermostat.py
@@ -7,6 +7,7 @@ except ImportError:
 
     CLOUD = True
 
+import time
 from copy import deepcopy
 from utilities import *
 
@@ -90,13 +91,15 @@ scheduleModeMap = {
 setHoldStatusMap = {
     0: 'NoHold',
     1: 'TemporaryHold',
-    2: 'PermanentHold',
+    2: 'PermanentHold'
 }
 
 holdStatusMap = {
     'NoHold': 0,
-    'HoldUntil': 1,
-    'PermanentHold': 2
+    'TemporaryHold': 1,
+    'PermanentHold': 2,
+    'HoldUntil': 3,
+    'VacationHold': 4
 }
 
 
@@ -120,12 +123,24 @@ class Thermostat(polyinterface.Node):
     def query(self):
         try:
             LOGGER.debug("Query thermostat {}".format(self.address))
-            thermostat = self._api.get_thermostat(self._location_id, self._thermostat_id)
+
+            # Sometimes the GET api doesn't update as quickly after an update. When this happens the heat/cool setpoints
+            # can show up as zero. So instead of displaying that to the user retry and see if we can get a good value
+            for i in range(10):
+                thermostat = self._api.get_thermostat(self._location_id, self._thermostat_id)
+                heat_setpoint = to_driver_value(thermostat.changeable_values.heat_setpoint, True)
+                cool_setpoint = to_driver_value(thermostat.changeable_values.cool_setpoint, True)
+
+                if heat_setpoint != 0 and cool_setpoint != 0:
+                    break
+
+                LOGGER.warning("Refreshing thermostat %s returned invalid heat/cool setpoints. Retry request #%s", self.address, (i+1))
+                time.sleep(0.5 * (i + 1))  # Incrementally back off the requests to give the API time to update
 
             updates = {
                 'ST': to_driver_value(thermostat.indoor_temperature, False),
-                'CLISPH': to_driver_value(thermostat.changeable_values.heat_setpoint, True),
-                'CLISPC': to_driver_value(thermostat.changeable_values.cool_setpoint, True),
+                'CLISPH': heat_setpoint,
+                'CLISPC': cool_setpoint,
                 'CLIMD': modeMap[thermostat.changeable_values.mode],
                 'CLIFS': fanMap[thermostat.settings.fan.changeable_values.mode],
                 'CLIHUM': to_driver_value(thermostat.indoor_humidity, True),
@@ -156,6 +171,7 @@ class Thermostat(polyinterface.Node):
 
         self.reportDrivers()
 
+    # setpoint - PH - Heat, PC - Cold, MD - Mode
     def cmdSetPF(self, cmd):
         try:
             driver = cmd['cmd']
@@ -166,7 +182,6 @@ class Thermostat(polyinterface.Node):
             c_setpoint = thermostat.changeable_values.cool_setpoint
             mode = thermostat.changeable_values.mode
             auto_changeover_active = thermostat.changeable_values.auto_changeover_active
-            next_period_time = thermostat.changeable_values.next_period_time
 
             updates = {
                 'CLISPH': to_driver_value(h_setpoint, True),
@@ -175,24 +190,28 @@ class Thermostat(polyinterface.Node):
             }
 
             if driver == 'CLISPH':
-                # There is a bug in the API when setting heat point. Basically TemporaryHold doesn't work.
-                # So instead we need to use HoldUntil with next period time which is essentially the same thing.
-                self._api.set_setpoint(self._location_id, self._thermostat_id, cmd['value'], c_setpoint, self._use_celsius, mode, auto_changeover_active, "HoldUntil", next_period_time)
+                LOGGER.debug("Setting heat setpoint for %s to %s", self.address, to_driver_value(cmd['value'], True))
+                self._api.set_setpoint(self._location_id, self._thermostat_id, cmd['value'], c_setpoint, self._use_celsius, mode, auto_changeover_active)
                 updates['CLISPH'] = to_driver_value(cmd['value'], True)
             elif driver == 'CLISPC':
+                LOGGER.debug("Setting cool setpoint for %s to %s", self.address, to_driver_value(cmd['value'], True))
                 self._api.set_setpoint(self._location_id, self._thermostat_id, h_setpoint, cmd['value'], self._use_celsius, mode, auto_changeover_active)
                 updates['CLISPC'] = to_driver_value(cmd['value'], True)
             elif driver == 'CLIMD':
                 mode = next((key for key, value in modeMap.items() if value == int(cmd['value'])), None)
+                LOGGER.debug("Setting mode for %s to %s", self.address, mode)
                 self._api.set_setpoint(self._location_id, self._thermostat_id, h_setpoint, c_setpoint, self._use_celsius, mode, auto_changeover_active)
                 updates['CLIMD'] = cmd['value']
 
-            for key, value in updates.items():
-                self.l_debug('_update', 'setDriver({},{})'.format(key, value))
-                self.setDriver(key, value)
+            # Setting these values can cause other things like Hold Status to change.
+            # Query the thermostat to get updated values.
+            self.query()
+
+            LOGGER.debug("Finished setting setpoint for %s", self.address)
         except Exception as ex:
             LOGGER.exception("Could not set thermostat set point %s because %s", self.address, ex)
 
+    # Schedule Mode (Hold Mode)
     def cmdSetHoldStatus(self, cmd):
         try:
             # Get current values so we don't change the wrong things
@@ -205,21 +224,26 @@ class Thermostat(polyinterface.Node):
             updates = {}
 
             host_status_mode = setHoldStatusMap[int(cmd['value'])]
+            LOGGER.debug("Setting hold mode for %s to %s", self.address, host_status_mode)
+
             self._api.set_setpoint(self._location_id, self._thermostat_id, h_setpoint, c_setpoint, self._use_celsius, mode, auto_changeover_active, host_status_mode)
             updates['GV4'] = cmd['value']
 
-            for key, value in updates.items():
-                self.l_debug('_update', 'setDriver({},{})'.format(key, value))
-                self.setDriver(key, value)
+            # Setting Hold Mode from a hold back to running can cause more than just the hold status to change.
+            # Query the thermostat to get updated values.
+            self.query()
 
+            LOGGER.debug("Finished setting hold mode for %s to %s", self.address, host_status_mode)
         except Exception as ex:
             LOGGER.exception("Could not set thermostat hold status %s because %s", self.address, ex)
 
+    # Fan Mode
     def cmdSetFS(self, cmd):
         try:
             updates = {}
 
             mode = next((key for key, value in fanMap.items() if value == int(cmd['value'])), None)
+            LOGGER.debug("Setting fan mode for %s to %s", self.address, mode)
             self._api.set_fanmode(self._location_id, self._thermostat_id, mode)
             updates['CLIFS'] = cmd['value']
 
@@ -227,6 +251,7 @@ class Thermostat(polyinterface.Node):
                 self.l_debug('_update', 'setDriver({},{})'.format(key, value))
                 self.setDriver(key, value)
 
+            LOGGER.debug("Finished setting fan mode for %s to %s", self.address, mode)
         except Exception as ex:
             LOGGER.exception("Could not set thermostat fan mode %s because %s", self.address, ex)
 
